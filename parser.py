82ca0943e0a09847d4ac89014c9b705c4ed16f93
Jack Adam <jackadam@Jacks-MBP-3.wireless.yale.internal>
2020-01-20 22:09:33 -0500

    added message if no unused classes/ids

--- parser.py
+++ parser.py
@@ -4,13 +4,20 @@ def main():
     c = find.parse_css()
     h = find.parse_html()
 
+    flag = False
+
     for x in c[0]:
         if x not in h[0]:
+            flag = True;
             print("Unused class: " + x)
 
     for y in c[1]:
         if y not in h[1]:
+            flag = True;
             print("Unused ID: " + y)
+
+    if flag == False:
+        print("No unused classes nor IDs!")
 
 if __name__ == "__main__":
     main()
